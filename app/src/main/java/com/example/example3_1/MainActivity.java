b31942e96acf7e3843a9560e6ab0fbf82eb9d510
jullystudio <jullystudio@naver.com>
2019-11-03 23:32:58 +0900

    taehun help me

--- app/src/main/java/com/example/example3_1/MainActivity.java
+++ app/src/main/java/com/example/example3_1/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,9 +28,28 @@ public class MainActivity extends AppCompatActivity {
         EditText_password = findViewById(R.id.EditText_password);
         Button_Login = findViewById(R.id.Button_Login);
 
-        // 1. 값을 가져온다
+        // 1. 값을 가져온다 - 임시 검사 기준: 2014430023, 19950511@)
         // 2. 클릭을 감지한다
         // 3. 1번의 값을 다음 액티비티로 넘긴다.
+
+        //ID, password가 일치하는지 체크
+        EditText_email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d("SENTI", s + "," + count);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         //클릭 가능 여부를 설정
         Button_Login.setClickable(true);
